863744b04530ab7941af82035424d1267bbb78e6
Fritz Mueller <fritzm@slac.stanford.edu>
2018-02-07 08:21:25 -0800

    Minor fixups
    - Keep build dir within package dir
    - Removed unnecessary addition of includes to PATH

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -1,14 +1,14 @@
 TAP_PACKAGE=1
 
 PKGDIR=$PWD
-BUILDDIR=$PWD/../antlr4-build
+BUILDDIR=$PWD/runtime/Cpp/build
 
 config()
 {
     rm -rf ${BUILDDIR}
     mkdir ${BUILDDIR}
     cd ${BUILDDIR}
-    cmake ${PKGDIR}/runtime/Cpp/ -DCMAKE_INSTALL_PREFIX=${PREFIX}
+    cmake .. -DCMAKE_INSTALL_PREFIX=${PREFIX}
 }
 
 build()
