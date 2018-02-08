7a2f6fa212bcc0769d4e127a01f011e62fa0c600
Fritz Mueller <fritzm@slac.stanford.edu>
2018-02-07 19:34:50 -0800

    Add antlr4 jar and script

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -3,6 +3,15 @@ TAP_PACKAGE=1
 PKGDIR=$PWD
 BUILDDIR=$PWD/runtime/Cpp/build
 
+prep()
+{
+    default_prep
+    mkdir -p ./share/java
+    mv ./*.jar ./share/java
+    mkdir -p ./bin
+    mv antlr4 ./bin
+}
+
 config()
 {
     rm -rf ${BUILDDIR}
@@ -22,5 +31,9 @@ install()
     cd ${BUILDDIR}
     make -j$NJOBS install
     cd ${PKGDIR}
+    mkdir -p "$PREFIX/share/java"
+    cp -f -a ./share/java/* "$PREFIX/share/java"
+    mkdir -p "$PREFIX/bin"
+    cp -f -a ./bin/* "$PREFIX/bin"
     install_ups
 }
